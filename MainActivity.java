bb2145ce173cc6958cce7bb8c9e3b26f07f63da1
rahomymedical-sketch <rahomymedical@gmail.com>
2025-09-13 19:21:07 +0300

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.mywebviewapp;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        webView = findViewById(R.id.webview);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        
+        // منع فتح الروابط في متصفح خارجي
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        
+        // استبدل الرابط برابط موقعك
+        webView.loadUrl("https://example.com");
+    }
+
+    // التعامل مع زر الرجوع
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
